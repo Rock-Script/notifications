1430253d9d7f198c153aa4f9d04994864cd1d1c8
Yogesh <yogesh>
2023-02-15 09:38:19 +0530

    member signup email

--- components/notification/notification.service.js
+++ components/notification/notification.service.js
@@ -6,7 +6,7 @@ const HTTP_RESPONSES = require('../../template/contants/http-responses');
 const EmailTemplateModel = require('../email/email_template.model');
 
 const verifyParams = async(params) => {
-    if (params.type === NOTIFICATION_TYPES.EMAIL) {
+    if (params.type.indexOf(NOTIFICATION_TYPES.EMAIL) > -1) {
         if (!params.email_template_id) {
             throw HTTP_RESPONSES.BAD_REQUEST('email_template_id is required');
         } else {
@@ -23,22 +23,28 @@ const verifyParams = async(params) => {
             throw HTTP_RESPONSES.NOT_FOUND('members', params.reciever_member_ids);
         }
     }
+
+    return params;
 }
 
 module.exports.send = async(params) => {
-    params = verifyParams(params);
+    params = await verifyParams(params);
     const to = params.reciever_members.map(m => m.email);
     const subject = stringSubstitutions(params.email_template.subject, params.data);
     const text = stringSubstitutions(params.email_template.text, params.data);
     const html = stringSubstitutions(params.email_template.html, params.data);
-    const mail_output = await Mailer.sendMail(to, subject, text, html);
-    console.log(`mail_output: ${mail_output}`);
+
+    const mail_output = [];
+    for (let t of to) {
+        mail_output.push(await Mailer.sendMail(t, subject, text, html));
+    }
+    console.log(`mail_output: ${JSON.stringify(mail_output)}`);
     return mail_output;
 }
 
 const stringSubstitutions = (string, map) => {
     _.keys(map).forEach(key => {
-        string.replace(/{{key}}/g, map[key]);
+        string = string.replaceAll(`{{${key}}}`, map[key]);
     })
     return string;
 }
